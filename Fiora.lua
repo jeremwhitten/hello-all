02629a1c6f93206fdc03e288f698b1531ae9fbf1
jeremwhitten <jeremyscooby@gmail.com>
2016-01-29 15:12:04 -0600

    Create Fiora.lua

---
+++ Fiora.lua
@@ -0,0 +1,38 @@
+if GetObjectName(GetMyHero()) ~= "Fiora" then return end
+require('Inspired')
+local mainMenu = Menu("Fiora", "Fiora")
+mainMenu:SubMenu("Combo", "Combo")
+mainMenu.Combo:Boolean("Q", "Use Q", true)
+mainMenu.Combo:Boolean("E", "Use E", true)
+mainMenu.Combo:Boolean("R", "Use R", true)
+
+mainMenu:SubMenu("Drawings", "Drawings:")
+mainMenu.Drawings:Boolean("Q","Draw Q", true)
+mainMenu.Drawings:Boolean("E","Draw E", true)
+mainMenu.Drawings:Boolean("R","Draw R", true)
+ 
+
+
+
+local target = GetCurrentTarget()
+
+
+OnTick(function(myHero)
+	local target = GetCurrentTarget()
+	if IOW:Mode() == "Combo" then
+	local target = GetCurrentTarget()
+                       
+					    local QPred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),0,0,400,250,false,true)
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 750) and mainMenu.Combo.Q:Value() then
+                        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+                        end
+
+                        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, 500) and mainMenu.Combo.R:Value() then
+                        CastTargetSpell(target, _R)
+                    	end
+			
+			if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 300) and mainMenu.Combo.E:Value() then
+                        CastSpell(_E)
+			end
+	end
+end)
