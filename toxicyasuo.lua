87f0686551e7e5be3bc6d8a05f4b19c69d2604ed
jeremwhitten <jeremyscooby@gmail.com>
2016-06-29 13:16:45 -0500

    Update toxicyasuo.lua

--- toxicyasuo.lua
+++ toxicyasuo.lua
@@ -3,10 +3,10 @@ if GetObjectName(GetMyHero()) ~= "Yasuo" then return end
 local YasuoMenu = Menu("Yasuo", "Yasuo")
 YasuoMenu:SubMenu("Combo", "Combo")
 YasuoMenu.Combo:KeyBinding("comboKey", "Combo Key", 32)
-YasuoMenu:Boolean("Q", "Use Q", true)
-YasuoMenu:Boolean("W", "Use W", true)
-YasuoMenu:Boolean("E", "Use E", true)
-YasuoMenu:Boolean("R", "Use R", true)
+YasuoMenu.Combo:Boolean("Q", "Use Q", true)
+YasuoMenu.Combo:Boolean("W", "Use W", true)
+YasuoMenu.Combo:Boolean("E", "Use E", true)
+YasuoMenu.Combo:Boolean("R", "Use R", true)
 
 YasuoMenu:Menu("Harass", "Harass")
 YasuoMenu.Harass:KeyBinding("harassKey", "Harass Key", string.byte("C"))
@@ -23,39 +23,69 @@ OnTick(function (myHero)
 local target = GetCurrentTarget()
 
 	if KeyIsDown(YasuoMenu.Combo.comboKey:Key()) then
+	
+		if YasuoMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 475) then
+		local QPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target),1500,250,425,90,false,false)
+		if QPred.HitChance == 1 then
+		CastSkillShot(_Q,QPred.PredPos)
+		end
+		end
+		
+		if YasuoMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 900) and GetCastName(myHero,_Q) == "YasuoQ3W" then
+		local QWPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target),1500,250,900,90,false,false)
+		if QWPred.HitChance == 1 then
+		CastSkillShot(_Q,QWPred.PredPos)
+		end
+		end
+		
+		
 		if YasuoMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 475) then
 			CastTargetSpell(target, _E)
 		end
 		
+		if YasuoMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 1200) then
+		if GetCurrentHP(target)+GetDmgShield(target) < CalcDamage(myHero, target, GetCastLevel(myHero,_R)+GetBonusDmg(myHero))then
+		CastSpell(_R)
+		end
+		end
+		
+		local TargetDistance = GetDistance(target)
+		mPos = ClosestMinion(target,MINION_ENEMY)
+		if IsReady(_E) and mPos then
+		CastTargetSpell(mPos, _E)
+		end
+		
 	end
 	
+	
+	
+	
+	for _,Y in pairs(minionManager.objects) do
+	if ValidTarget(Y,475) then
 
+	for _, Y in pairs(minionManager.objects) do
+	if ValidTarget(Y,475) then 
+	local z = (GetCastLevel(myHero,_E)*20)+(GetBonusAP(myHero)*.65)+(GetBaseDamage(myHero))
+	local hp = GetCurrentHP(Y)
+	local Dmg = CalcDamage(myHero, Y, z)
+		if KeyIsDown(YasuoMenu.LaneClear.laneclearKey:Key())then
+			if YasuoMenu.LaneClear.E:Value() and dmg > hp then
+			CastTargetSpell(Y, _E)
+			end
+		end
+	end
+	end
+	end
+	
+	
+	end
+	
 	for i,mobs in pairs(minionManager.objects) do
 		if KeyIsDown(YasuoMenu.LaneClear.laneclearKey:Key()) then
 			if YasuoMenu.LaneClear.Q:Value() and ValidTarget(mobs,475)then
 			CastSkillShot(_Q,mobs)
 			end
 		end
-	end
-	
-	for _,M in pairs(minionManager.objects) do
-	if ValidTarget(M,475) then
-
-	for _, M in pairs(minionManager.objects) do
-	if ValidTarget(M,475) then 
-	local z = (GetCastLevel(myHero,_E)*20)+(GetBonusAP(myHero)*.70)+(GetBaseDamage(myHero))
-	local hp = GetCurrentHP(M)
-	local Dmg = CalcDamage(myHero, M, z)
-		if KeyIsDown(YasuoMenu.LaneClear.laneclearKey:Key())then
-			if YasuoMenu.LaneClear.E:Value() and dmg > hp then
-			CastTargetSpell(M, _E)
-			end
-		end
-	end
-	end
-	end
-	
-	
 	end
 	
 	
