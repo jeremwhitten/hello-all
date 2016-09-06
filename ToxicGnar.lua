64f7f4fdbbbfc1252fba704454d939f3ecde2223
jeremwhitten <jeremyscooby@gmail.com>
2016-09-06 11:15:04 -0500

    Update and rename Toxic Gnar.lua to ToxicGnar.lua

--- ToxicGnar.lua
+++ ToxicGnar.lua
@@ -30,7 +30,7 @@ if KeyIsDown(GnarMenu.Combo.comboKey:Key()) then
 	end
 		
 	if GnarMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1100) then
-		if GetCastName(myHero,_Q) == "gnarbigq" and GotBuff(myHero, "gnartransform") == 1 then
+		if GetCastName(myHero,_Q) == "GnarBigQ" and GotBuff(myHero, "gnartransform") == 1 then
 		 local QPred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target),2100,100,1100,90,true,true)
 		 if QPred.HitChance == 1 then
 		 CastSkillShot(_Q,QPred.PredPos)
@@ -40,7 +40,7 @@ if KeyIsDown(GnarMenu.Combo.comboKey:Key()) then
 		
 		
 	if GnarMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 525) then
-		if GetCastName(myHero,_W) == "gnarbigw" and GotBuff(myHero, "gnartransform") == 1 then
+		if GetCastName(myHero,_W) == "GnarBigW" and GotBuff(myHero, "gnartransform") == 1 then
 		 local WPred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target),600,250,525,90,false,true)
 		 if WPred.HitChance == 1 then
 		 CastSkillShot(_W,WPred.PredPos)
@@ -58,7 +58,7 @@ if KeyIsDown(GnarMenu.Combo.comboKey:Key()) then
 	end
 	
 	if GnarMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 500) then
-		if GetCastName(myHero,_W) == "gnarbige" and GotBuff(myHero, "gnartransform") == 1 then
+		if GetCastName(myHero,_W) == "GnarBigE" and GotBuff(myHero, "gnartransform") == 1 then
 		 local EPred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target),1500,100,500,475,false,false)
 		 if EPred.HitChance == 1 then
 		 CastSkillShot(_E,EPred.PredPos)
@@ -66,21 +66,35 @@ if KeyIsDown(GnarMenu.Combo.comboKey:Key()) then
 		end	
 	end
 	
+	if GnarMenu.Combo.R:Value()then
+	self:CastR()
+	end
+	
+	
+	
+	
+	function Gnar:CastR()
+	
 	for _,unit in pairs(GetEnemyHeroes()) do
 		local distance=590 - GetDistance(myHero,unit)
 		local RPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),3000,0,590,590,false,true)
         local PredPos = Vector(RPred.PredPos)
         local HeroPos = Vector(myHero)
         local maxRRange = PredPos - (PredPos - HeroPos) * ( - distance / GetDistance(RPred.PredPos))
+		local shootLine = Line(Point(PredPos.x, PredPos.y, PredPos.z), Point(maxRRange.x, maxRRange.y, maxRRange.z))
+	for i, Pos in pairs(shootLine:__getPoints()) do
+		
+		
 		
 		if MapPosition:inWall(Pos) and Ready(_R) and ValidTarget(target,590) and GetDistance(myHero,unit) <= 590 then
 		CastSkillShot(_R,RPred.PredPos)
 		end
+	end
 	end
 	
  end
  
 end)
 
-print ("Loaded") 
+print ("Toxic Gnar") 
 	
